f9a21f6c6fe6ab2a2c057ba438f99d7a4208a438
dinethde <dinethran33@gmail.com>
2025-11-03 21:28:18 +0530

    feat: highlights "mosaic" as external GitHub link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import { RefreshCcw } from "lucide-react";
-import { ChevronRight } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans text-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col justify-between py-20 px-16 sm:items-start">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col gap-10 justify-between py-20 px-16 sm:items-start s">
         <div className="flex flex-col gap-1">
           <h1 className="text-[1rem] font-mono font-normal">
             Dineth Ransandu Silva
@@ -48,15 +47,36 @@ export default function Home() {
         </div>
 
         <div>
-          <div className="flex gap-2 items-center font-mono font-normal cursor-heart">
-            <p className="hover:text-[#ff7300] transition-colors duration-300">I’m</p>
-            <p className="hover:text-[#ff7300] transition-colors duration-300">a</p>
-            <p className="hover:text-[#ff7300] transition-colors duration-300">mosaic</p>
-            <p className="hover:text-[#ff7300] transition-colors duration-300">of</p>
-            <p className="hover:text-[#ff7300] transition-colors duration-300">everyone</p>
-            <p className="hover:text-[#ff7300] transition-colors duration-300">I’ve</p>
-            <p className="hover:text-[#ff7300] transition-colors duration-300">ever</p>
-            <p className="hover:text-[#ff7300] transition-colors duration-300">loved.</p>
+          <div className="flex gap-2 items-center font-mono font-normal cursor-heart flex-wrap">
+            <p className="hover:text-[#ff7300] transition-colors duration-300 ">
+              I’m
+            </p>
+            <p className="hover:text-[#ff7300] transition-colors duration-300">
+              a
+            </p>
+            <Link
+              className="text-highlighted"
+              target="_blank"
+              href={"https://www.github.com/dinethde"}
+              rel="noopener noreferrer"
+            >
+              mosaic
+            </Link>
+            <p className="hover:text-[#ff7300] transition-colors duration-300">
+              of
+            </p>
+            <p className="hover:text-[#ff7300] transition-colors duration-300">
+              everyone
+            </p>
+            <p className="hover:text-[#ff7300] transition-colors duration-300">
+              I’ve
+            </p>
+            <p className="hover:text-[#ff7300] transition-colors duration-300">
+              ever
+            </p>
+            <p className="hover:text-[#ff7300] transition-colors duration-300">
+              loved.
+            </p>
           </div>
 
           <p className="font-fuggles text-[4rem]">Dineth De Silva</p>
